fe0fc815de94df9e38f352a8e5418049f78a9981
Daniel Zavala <siedrix@gmail.com>
2025-03-07 14:23:10 -0600

    Updated sample task

--- apps/sample-project/src/tasks/stock/getPrice.ts
+++ apps/sample-project/src/tasks/stock/getPrice.ts
@@ -1,26 +1,39 @@
 // TASK: getPrice
 // Run this task with:
-// shadow-cli task:run stock:getPrice
+// shadow-cli task:run stock:getPrice --ticker AAPL
+// shadow-cli task:run stock:getPrice --ticker VOO
 
 import { createTask } from '@shadow/task'
 import { Schema } from '@shadow/schema'
 
 const schema = new Schema({
-  // Add your schema definitions here
-  // example: myParam: Schema.string()
+  ticker: Schema.string()
 })
 
 const boundaries = {
-  // Add your boundary functions here
-  // example: readFile: async (path: string) => fs.readFile(path, 'utf-8')
+  fetchStockPrice: async (ticker: string): Promise<{price:number}> => {
+    const url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}`
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch stock price for ${ticker}: ${response.statusText}`)
+    }
+
+    const data = await response.json()
+    const price = data.chart.result[0].meta.regularMarketPrice
+    return {price}
+  }
 }
 
 export const getPrice = createTask(
   schema,
   boundaries,
-  async function (_argv, _boundary) {
-    const status = { status: 'Its alive!!!!' }
+  async function ({ ticker }, { fetchStockPrice }): Promise<{ ticker: string, price: number }> {
+    const { price } = await fetchStockPrice(ticker as string)
 
-    return status
+    return {
+      ticker,
+      price
+    }
   }
 )
