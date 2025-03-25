68fc47ccd0d68e6fff83b175d61b5799d7191af1
Daniel Zavala <siedrix@gmail.com>
2025-03-24 22:58:48 -0600

    Added a simple react/tailwind deploy that works

--- apps/web-page/astro.config.mjs
+++ apps/web-page/astro.config.mjs
@@ -3,14 +3,25 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
 
+  integrations: [
+    react(),
+  ],
+
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
+    resolve: {
+      // https://github.com/facebook/react/issues/31827
+      alias: import.meta.env.PROD ? {
+        'react-dom/server': 'react-dom/server.edge'
+      } : undefined
+    }
   },
 });
