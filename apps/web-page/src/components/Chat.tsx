1f295d274f6294350366cf742ff3ad076193db9e
Daniel Zavala <siedrix@gmail.com>
2025-06-09 11:07:47 -0600

    Remove web page app since its on another repo/stack now

--- apps/web-page/src/components/Chat.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Chat: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center px-4">
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Lets start chatting...</button>
-    </div>
-  )
-}
