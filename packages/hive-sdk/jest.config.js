f656b3232bbe462c8e688d204521ba64ed6c3aa9
Daniel Zavala <siedrix@gmail.com>
2025-06-18 20:45:38 -0600

    Added hive sdk setup

---
+++ packages/hive-sdk/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/src/test/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node']
+} 
---
+++ packages/hive-sdk/src/test/index.test.ts
@@ -0,0 +1,5 @@
+describe('Hive SDK', () => {
+  it('should be defined', () => {
+    expect(true).toBe(true)
+  })
+})
